c3f42a758b132812caf7a36d9816d04ccaae3ab1
Umair Hussain <bc230203492uhu@vu.edu.pk>
2024-09-22 00:41:58 +0500

    upload book files to cloudinary

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,7 +1,48 @@
 import { NextFunction, Request, Response } from "express";
+import cloudinary from "../config/cloudinary";
+import path from "node:path";
 
-const createBook = (req: Request, res: Response, next: NextFunction) => {
-    res.json({ message: "Book Listing Created Successfully!" });
+const createBook = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        // Uplaod Cover-Image to Cloudinary
+        const files = req.files as {
+            [fieldname: string]: Express.Multer.File[];
+        };
+        const coverImageMimeType = files.coverImage[0].mimetype
+            .split("/")
+            .at(-1);
+        const fileName = files.coverImage[0].filename;
+        const filePath = path.join(
+            __dirname,
+            "../../public/data/uploads",
+            fileName,
+        );
+        const uploadCoverImage = await cloudinary.uploader.upload(filePath, {
+            filename_override: fileName,
+            folder: "book-cover",
+            format: coverImageMimeType,
+        });
+
+        // Uplaod book-file to Cloudinary
+        const bookFileName = files.file[0].filename;
+        const bookFilePath = path.join(
+            __dirname,
+            "../../public/data/uploads",
+            bookFileName,
+        );
+
+        const uploadBookFile = await cloudinary.uploader.upload(bookFilePath, {
+            resource_type: "raw",
+            filename_override: bookFileName,
+            folder: "book-pdfs",
+            format: "pdf",
+        });
+        console.log("uploadBookFile ", uploadBookFile);
+
+        res.json({ message: "Book Listing Created Successfully!" });
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export { createBook };
